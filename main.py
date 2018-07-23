b3132bec8a8ec06a7da7109353853cf592e03f09
Yury Garnov <hamta@yandex.ru>
2018-07-23 15:02:16 +0300

    Update main.py

--- main.py
+++ main.py
@@ -48,26 +48,22 @@ app_id = 6630979
 
 #         return 'ok'
 
-# @app.route('/', methods=['POST'])
-# def processing():
-#         #Распаковываем json из пришедшего POST-запроса
-#     data = json.loads(request.data)
-#     #Вконтакте в своих запросах всегда отправляет поле типа
-#     if 'type' not in data.keys():
-#         return 'not vk'
-#     if data['type'] == 'confirmation':
-#         return confirmation_token
-#     elif data['type'] == 'message_new':
-#         session = vk.Session()
-#         api = vk.API(session, v=5.0)
-#         user_id = data['object']['user_id']
-#         api.messages.send(access_token=token, user_id=str(user_id), message='Привет, я новый бот!')
-#         # Сообщение о том, что обработка прошла успешно
-#         return 'ok'
-
 @app.route('/', methods=['POST'])
 def processing():
-      return confirmation_token  
+        #Распаковываем json из пришедшего POST-запроса
+    data = json.loads(request.data)
+    #Вконтакте в своих запросах всегда отправляет поле типа
+    if 'type' not in data.keys():
+        return 'not vk'
+    if data['type'] == 'confirmation':
+        return confirmation_token
+    elif data['type'] == 'message_new':
+        session = vk.Session()
+        api = vk.API(session, v=5.0)
+        user_id = data['object']['user_id']
+        api.messages.send(access_token=token, user_id=str(user_id), message='Привет, я новый бот!')
+        # Сообщение о том, что обработка прошла успешно
+        return 'ok'
 
 @app.route('/')
 def LOL():
