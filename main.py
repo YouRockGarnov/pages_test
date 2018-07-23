47ede55f010969f9d8f30da085c1f6f63cfa98e3
Yury Garnov <hamta@yandex.ru>
2018-07-23 19:03:52 +0300

    Working

--- main.py
+++ main.py
@@ -48,27 +48,35 @@ app_id = 6630979
 
 #         return 'ok'
 
-# @app.route('/', methods=['POST'])
-# def processing():
-#         #Распаковываем json из пришедшего POST-запроса
-#     data = json.loads(request.data)
-#     #Вконтакте в своих запросах всегда отправляет поле типа
-#     if 'type' not in data.keys():
-#         return 'not vk'
-#     if data['type'] == 'confirmation':
-#         return confirmation_token
-#     elif data['type'] == 'message_new':
-#         session = vk.Session()
-#         api = vk.API(session, v=5.0)
-#         user_id = data['object']['user_id']
-#         api.messages.send(access_token=token, user_id=str(user_id), message='Привет, я новый бот!')
-#         # Сообщение о том, что обработка прошла успешно
-#         return 'ok'
-
 @app.route('/', methods=['POST'])
 def processing():
-      return confirmation_token  
+    return 'lol'
+    session = vk.Session()
+    api = vk.API(session, v=5.0)
+    api.send_message(481116745, token, 'Это я')
+    return 'ok'
+    
+    
+    user_id = data['object']['user_id']
+    message = data['object']['title']
+        
+        #Распаковываем json из пришедшего POST-запроса
+    data = json.loads(request.data)
+    #Вконтакте в своих запросах всегда отправляет поле типа
+    if 'type' not in data.keys():
+        return 'not vk'
+    if data['type'] == 'confirmation':
+        return confirmation_token
+    elif data['type'] == 'message_new':
+        session = vk.Session()
+        api = vk.API(session, v=5.0)
+        user_id = data['object']['user_id']
+        api.messages.send(access_token=token, user_id=str(user_id), message='Привет, я новый бот!')
+        # Сообщение о том, что обработка прошла успешно
+        return 'ok'
 
 @app.route('/')
 def LOL():
-    return confimation_token
+    return 'hello'
+
+app.run(debug=True)
