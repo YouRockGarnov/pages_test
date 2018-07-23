9619bbd9a29b622956c10e49e373cab7c97d840b
Yury Garnov <hamta@yandex.ru>
2018-07-23 18:30:12 +0300

    Update main.py

--- main.py
+++ main.py
@@ -79,4 +79,7 @@ def processing():
 def LOL():
     return 'hello'
 
-app.run(debug=True)
+if __name__ == '__main__':
+    app.debug = True
+    port = int(os.environ.get('PORT',5000))
+    app.run(host='0.0.0.0', port=port)
